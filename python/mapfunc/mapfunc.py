73c9c93ec03f7111793cd14a02a30c9b693ab77e
Harsh4r0ra <harsh.arora0605+git@gmail.com>
2024-09-26 15:46:27 +0530

    map filter diff

--- python/mapfunc/mapfunc.py
+++ python/mapfunc/mapfunc.py
@@ -1,7 +1,6 @@
 # Example list
 numbers = [1, 2, 3, 4, 5]
 
-# Use map with a lambda to square each number in the list
 squared_numbers = list(map(lambda x: x ** 2, numbers))
 
 print(squared_numbers)
